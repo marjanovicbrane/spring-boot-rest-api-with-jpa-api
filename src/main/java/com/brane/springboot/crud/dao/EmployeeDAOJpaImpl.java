8267fd8d0cebeb6f6faf54528aca2769a58231ff
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-07-11 13:43:31 +0200

    We created a new class EmployeeDAOJpaImpl becase we are using now STANDARD JPA API.In this class we also created a new method findAll() which show us all employee objects from the database using STANDARD JPA API.

---
+++ src/main/java/com/brane/springboot/crud/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,59 @@
+package com.brane.springboot.crud.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.brane.springboot.crud.entity.Employee;
+
+//THIS IS DAO LAYER
+@Repository
+public class EmployeeDAOJpaImpl implements EmployeeDAO {
+
+	//private field entityManager, so we can do constructor dependency injection
+	private EntityManager entityManager;
+			
+	//constructor injection
+	@Autowired
+	public EmployeeDAOJpaImpl(EntityManager theEntityManager) {
+	entityManager = theEntityManager;
+	}
+			
+			
+	@Override
+	public List<Employee> findAll() {
+		
+		//create a query
+		//WE ARE USING HERE STANDARD JPA API and JPQL
+		Query query=entityManager.createQuery("from Employee");
+		
+		// execute query and get result list
+		List<Employee> employees=query.getResultList();
+		
+		// return the results	
+		return employees;
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
